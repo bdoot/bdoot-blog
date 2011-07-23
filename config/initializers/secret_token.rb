472eab302fd50d9f25d240f7fd92d87afb01391d
Brandon Doot <brandon.doot@gmail.com>
2011-07-22 21:03:40 -0400

    added rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BdootBlog::Application.config.secret_token = '23ced5cf10f3fef0953a24082ad27312808ece7b54f8699f59f9a14bdcf8ee6ee91fd77e57022894169165d23599066d25c719ef34ef4469b407226771d5fef9'
